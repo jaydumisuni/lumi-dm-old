d413043b2d261426fb4fb763be11d9e4847dd8d0
THE TECH GUY <65334711+jaydumisuni@users.noreply.github.com>
2026-07-23 20:17:26 +0200

    Load final startup capacity and notification reliability

--- electron/bootstrap-v5-final.js
+++ electron/bootstrap-v5-final.js
@@ -3,5 +3,8 @@
 require("./native-session-v5");
 require("./ttg-shell-bootstrap");
 require("./legacy-guards-v5");
+require("./server-supervisor-v6");
+require("./notification-baseline-v6");
+require("./connection-capacity-v6");
 require("./desktop-command-poller");
 require("./bootstrap-v5");
